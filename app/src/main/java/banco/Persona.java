6b3208fda13920a59d75066007073fd23b777bdf
d3lt409 <manuelfer1996@gmail.com>
2023-09-23 12:25:29 -0500

    add:Assets Diagrama UML

--- app/src/main/java/banco/Persona.java
+++ app/src/main/java/banco/Persona.java
@@ -2,9 +2,8 @@ package banco;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.text.DateFormat;
+
 
 public class Persona {
     
